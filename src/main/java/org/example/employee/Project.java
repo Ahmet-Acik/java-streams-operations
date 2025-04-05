76fe63b1d4d5d4006ac82f987a54667c46f09ae8
drahmetacik <a.acik@icloud.com>
2025-04-05 12:14:46 +0100

    Add Project enum to represent various project types

---
+++ src/main/java/org/example/employee/Project.java
@@ -0,0 +1,10 @@
+package org.example.employee;
+
+public enum Project {
+    CLOUD_MIGRATION,
+    DATA_ANALYTICS,
+    MOBILE_APP_DEVELOPMENT,
+    CYBER_SECURITY,
+    AI_RESEARCH,
+    DEVOPS_AUTOMATION
+}
